1dd96e0563dd1b83b18322ceb39bf9ff2463fa48
Mihai-Iusztin <minai.iusztin@gmail.com>
2024-02-13 13:28:52 +0200

    Input Component and input values

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,46 @@
 import Input from './components/Input';
+import { useState } from 'react';
+
 function App() {
+  const [instrumentInput, setInstrumentInput] = useState({
+    instrument: 'SGRY',
+  });
+  const [newInput, setNewInput] = useState({
+    // instrument: 'AMD',
+    initialStocks: 10,
+    initialPrice: 136.55,
+    annualProfitability: 22,
+    annualNewStocks: 5,
+    duration: 10,
+  });
+
+  function handleInstrument(newInstrument) {
+    setInstrumentInput((prevInstr) => {
+      return {
+        ...prevInstr,
+        instrument: newInstrument,
+      };
+    });
+  }
+
+  function handleInput(identifier, newValue) {
+    setNewInput((prevInput) => {
+      return {
+        ...prevInput,
+        [identifier]: +newValue,
+      };
+    });
+  }
+  console.log(newInput);
+  console.log(instrumentInput);
   return (
     <>
-      <Input />
+      <Input
+        investorInput={newInput}
+        inputChange={handleInput}
+        instrumentChange={handleInstrument}
+        instrumentInput={instrumentInput}
+      />
     </>
   );
 }
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,34 +1,79 @@
-export default function Input() {
+export default function Input({
+  investorInput,
+  instrumentInput,
+  inputChange,
+  instrumentChange,
+}) {
   return (
     <section id="input-container">
       <div className="user-input">
         <p>
           <label htmlFor="">Instrument</label>
-          <input type="text" required />
+          <input
+            type="text"
+            required
+            value={instrumentInput.instrument}
+            onChange={(event) => instrumentChange(event.target.value)}
+          />
         </p>
       </div>
       <div className="user-input">
         <p>
           <label htmlFor="">Number of stocks</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={investorInput.initialStocks}
+            onChange={(event) =>
+              inputChange('initialStocks', event.target.value)
+            }
+          />
         </p>
         <p>
           <label htmlFor="">Stock price</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            step="0.01"
+            value={investorInput.initialPrice}
+            onChange={(event) =>
+              inputChange('initialPrice', event.target.value)
+            }
+          />
         </p>
       </div>
       <div className="user-input">
         <p>
           <label htmlFor="">Stock variation %/year</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            step="0.01"
+            value={investorInput.annualProfitability}
+            onChange={(event) =>
+              inputChange('annualProfitability', event.target.value)
+            }
+          />
         </p>
         <p>
           <label htmlFor="">New investment(stks/y)</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={investorInput.annualNewStocks}
+            onChange={(event) =>
+              inputChange('annualNewStocks', event.target.value)
+            }
+          />
         </p>
         <p>
           <label htmlFor="">Duration(years)</label>
-          <input type="number" required />
+          <input
+            type="number"
+            required
+            value={investorInput.duration}
+            onChange={(event) => inputChange('duration', event.target.value)}
+          />
         </p>
       </div>
     </section>
